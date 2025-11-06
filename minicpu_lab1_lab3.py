bd179608cb207494b69ec4583ee1df179cf6aca7
SirFerr <ursusmartimus007@gmail.com>
2025-11-06 17:14:54 +0300

    Update minicpu_lab1_lab3.py

--- minicpu_lab1_lab3.py
+++ minicpu_lab1_lab3.py
@@ -320,7 +320,7 @@ def assemble_and_run_demo(array: List[int], trace: bool = True) -> Tuple[List[in
     return code, final_state
 
 if __name__ == "__main__":
-    # Example: sum of [5, -2, 10, 7] = 20
+    # Example: sum of [5, -2, 112, 7] = 122
     program, state = assemble_and_run_demo([5, -2, 112, 7], trace=True)
     print("\n--- Machine code (hex) ---")
     print(" ".join(f"{b:02X}" for b in program))
